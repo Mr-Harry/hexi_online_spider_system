a297a6a3744d0098a9cc193fbcf188ddf7c1ea94
“Xuenew” <18188108851@163.com>
2021-02-04 17:53:40 +0800

    #操作修改记录 2021 -02 - 04 周四 1，微博分类 ✅     综合 1，     视频 64，。。。其余在 响应中cardlist_head_cards 这个字段有体现 很多分类 在请求的url中 修改 2,添加微博综合的抓取 在搜索引擎类里进行抓取 ✅     修改引擎类配置文件 3，修改工具文件 ✅     修改搜索引擎的sql  存入qinquan_id_str         为后期微博 详情来铺垫 4，添加本地测试第二个 ✅     audio_tongbu_main_all_test_xyy.py 5，配置文件修改 添加第二个队列 ✅     redis_task_set_qq_name_bendi_test_xyy

--- hexi_online_spider_v001/engine_search_unit/engine_function_unit.py
+++ hexi_online_spider_v001/engine_search_unit/engine_function_unit.py
@@ -10,4 +10,5 @@ __engine_search_function__ = {
             "4":"engine_search_unit.sougou_engine_unit.sougou_engine_search", # 搜狗搜索
             "5":"engine_search_unit.jinri_toutiao_news_search_unit.jinri_toutiao_zhannei_search_cartoon_search", # 今日头条文章搜索
             "6":"engine_search_unit.beijingshijian_news_search.beijin_shijian_search", # 北京时间文章搜索
+            "7":"engine_search_unit.weibo_zonghe_news_search_unit.weibo_zonghe_search", # 微博综合的搜索
 }
